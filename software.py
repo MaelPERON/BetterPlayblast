7734004a9c87b69d4348db0532c475863ff1ad55
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-13 23:55:58 +0200

    update(software.py) remove unnecessary icon_identifier method and clean up simple_icon assignment

--- software.py
+++ software.py
@@ -1,13 +1,7 @@
 class Software:
 	def __init__(self, name: str, simple_icon: str = None):
 		self.name = name
-		self.simple_icon = f"https://raw.githubusercontent.com/simple-icons/simple-icons/refs/heads/develop/icons/{self.icon_identifier}.svg" if simple_icon else None
-
-	@classmethod
-	def icon_identifier(cls, identifier: str):
-		name = identifier.split("/")[-1]
-		stem = ".".join(name.split(".")[:-1])
-		return stem
+		self.simple_icon = f"https://raw.githubusercontent.com/simple-icons/simple-icons/refs/heads/develop/icons/{simple_icon}.svg" if simple_icon else None
 
 class SoftwareList:
 	BLENDER = Software("blender", simple_icon="blender")
