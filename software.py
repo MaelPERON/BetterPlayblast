771ab42e3afd9ba1ff9c3fa6e36743d058d5e29f
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-13 16:37:27 +0200

    add(Software) icon_identifier class method

--- software.py
+++ software.py
@@ -1,3 +1,9 @@
 class Software:
 	def __init__(self, name: str, simple_icon: str = None):
 		self.name = name
+
+	@classmethod
+	def icon_identifier(cls, identifier: str):
+		name = identifier.split("/")[-1]
+		stem = ".".join(name.split(".")[:-1])
+		return stem
