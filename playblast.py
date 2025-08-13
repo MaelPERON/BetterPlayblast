5409788c593985d30f3161b379112cc67666f67a
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-13 17:01:09 +0200

    update(Playblast) now supports metadatas in constructor

--- playblast.py
+++ playblast.py
@@ -1,13 +1,16 @@
-from pathlib import Path
 import json
+from pathlib import Path
+from metadata import MetadataList as Metadata
 
 class Playblast:
-	def __init__(self, video_file: Path | str, json_file: Path | str):
+	def __init__(self, video_file: Path | str, json_file: Path | str, metadatas: list[Metadata] = None):
 		self.video_file = Path(video_file)
 		self.json_file = Path(json_file)
 		self.data = self._load_json()
 		if not self.data:
 			raise ValueError(f"JSON file {self.json_file} is empty.")
+
+		self.metadatas = metadatas or []
 
 	def _load_json(self):
 		if not self.json_file.exists():
