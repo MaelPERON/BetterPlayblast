d9cbb3d8345ee83914df384ef564124166f1f52d
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-13 16:37:38 +0200

    update(Software) initialize simple_icon with icon URL if provided

--- software.py
+++ software.py
@@ -1,6 +1,7 @@
 class Software:
 	def __init__(self, name: str, simple_icon: str = None):
 		self.name = name
+		self.simple_icon = f"https://raw.githubusercontent.com/simple-icons/simple-icons/refs/heads/develop/icons/{self.icon_identifier}.svg" if simple_icon else None
 
 	@classmethod
 	def icon_identifier(cls, identifier: str):
