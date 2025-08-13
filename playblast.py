f097b4ed1c380baa8968dc51d1d598e19a462b1d
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-13 17:16:30 +0200

    update(Playblast) add output_file parameter to Playblast constructor

--- playblast.py
+++ playblast.py
@@ -3,9 +3,10 @@ from pathlib import Path
 from metadata import MetadataList, Metadata
 
 class Playblast:
-	def __init__(self, video_file: Path | str, json_file: Path | str, metadatas: list[Metadata] = None):
+	def __init__(self, video_file: Path | str, json_file: Path | str, output_file: Path | str, metadatas: list[Metadata] = None):
 		self.video_file = Path(video_file)
 		self.json_file = Path(json_file)
+		self.output_file = Path(output_file)
 		self.data = self._load_json()
 		if not self.data:
 			raise ValueError(f"JSON file {self.json_file} is empty.")
