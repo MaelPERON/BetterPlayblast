748f2eccc7ddba5c6fff3eac819e4faa63dd14c5
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-15 01:11:33 +0200

    feat(install) implement list_installed_modules function to check installed packages

--- install.py
+++ install.py
@@ -1,3 +1,4 @@
+import importlib
 PACKAGES = {
 	"pillow": "PIL",
 	"opencv-python": "cv2",
@@ -5,3 +6,13 @@ PACKAGES = {
 	"PyYAML": "yaml",
 	"pyppeteer": "pyppeteer"
 }
+
+def list_installed_modules() -> list[str]:
+	installed_modules = []
+	for package in PACKAGES.values():
+		try:
+			importlib.import_module(package)
+			installed_modules.append(package)
+		except ImportError:
+			pass
+	return installed_modules
