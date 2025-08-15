b06cbd115c770295cb8fa368306fd6e71a198988
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-15 03:43:00 +0200

    feat(modules) update list_installed_modules and related functions to support caching and refresh options

--- install.py
+++ install.py
@@ -7,7 +7,13 @@ PACKAGES = {
 	"pyppeteer": "pyppeteer"
 }
 
-def list_installed_modules() -> list[str]:
+cached_modules = []
+
+def list_installed_modules(refresh: bool = True, save_cache: bool = True) -> list[str]:
+	global cached_modules
+	if not refresh and cached_modules:
+		return cached_modules
+
 	installed_modules = []
 	for package in PACKAGES.values():
 		try:
@@ -15,16 +21,18 @@ def list_installed_modules() -> list[str]:
 			installed_modules.append(package)
 		except ImportError:
 			pass
+
+	if save_cache: cached_modules = installed_modules
 	return installed_modules
 
-def missing_modules() -> list[str]:
-	installed_modules = list_installed_modules()
+def missing_modules(refresh: bool = True, save_cache: bool = True) -> list[str]:
+	installed_modules = list_installed_modules(refresh=refresh, save_cache=save_cache)
 	return [pkg for pkg in PACKAGES.values() if pkg not in installed_modules]
 
-def missing_packages() -> list[str]:
-	installed_modules = list_installed_modules()
+def missing_packages(refresh: bool = True, save_cache: bool = True) -> list[str]:
+	installed_modules = list_installed_modules(refresh=refresh, save_cache=save_cache)
 	return [key for key, value in PACKAGES.items() if value not in installed_modules]
 
-def all_installed() -> bool:
-	installed_modules = list_installed_modules()
+def all_installed(refresh: bool = True, save_cache: bool = True) -> bool:
+	installed_modules = list_installed_modules(refresh=refresh, save_cache=save_cache)
 	return len(installed_modules) == len(PACKAGES)
